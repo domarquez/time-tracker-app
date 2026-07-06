ae72afa6458b688078223e3d36ffe1497b4af321
domarquez <66529789+domarquez@users.noreply.github.com>
2026-07-06 08:31:10 -0400

    Update service worker cache name and structure

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'time-tracker-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE_NAME = 'control-horas-v1';
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
