030fe6c82518f4c875cbb622d04d9501c879d4f1
domarquez <66529789+domarquez@users.noreply.github.com>
2026-07-06 08:56:47 -0400

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-const CACHE_NAME = 'control-horas-v1';
+const CACHE_NAME = 'control-horas-v2';
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['/']))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
